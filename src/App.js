c5760cbb124cd048472d6c329f1622580584c316
m26unknw <shukla9915294376@gmail.com>
2021-01-09 14:34:06 +0530

    Added some Comopnents

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+import {IncomeExpenses} from './components/IncomeExpenses';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <div className="container">
+      <Balance />
+      <IncomeExpenses />
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Balance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Balance=()=>{
+    return(
+        <>
+        <h4>Your Balance</h4>
+        <h1 id='Balance'>₹0.0</h1>
+        </>
+
+    )
+}
